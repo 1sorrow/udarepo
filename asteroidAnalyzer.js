059c10f049df123fe1c5c18de037a95039ec58c4
ahmed hammam <hammam.m.ahmed@gmail.com>
2025-02-07 10:49:42 +0200

    Update asteroidAnalyzer.js
    added comments to make the code better to read and understand

--- asteroidAnalyzer.js
+++ asteroidAnalyzer.js
@@ -1,12 +1,15 @@
+// function for making  sure that we have data for the astroid
 function analyzeAsteroids(diameters) {
     if (!diameters || diameters.length === 0) {
         console.log("No asteroid data available.");
         return;
     }
 
+    // making variables to store the data
     const totalAsteroids = diameters.length;
     const averageDiameter = diameters.reduce((sum, diameter) => sum + diameter, 0) / totalAsteroids;
 
+    // printing the output
     console.log(`Total Asteroids: ${totalAsteroids}`);
     console.log(`Average Diameter: ${averageDiameter.toFixed(2)} km`);
 }
